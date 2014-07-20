0a35cb7361dd4e99a99279c2ffe516fda28c3f3e
Lemming <evil-lemming@users.noreply.github.com>
2014-07-20 00:21:08 +0000

    Texturen und Blend-Datei für Credits

---
+++ resources/credits/rendertext.c
@@ -0,0 +1,21 @@
+#include <acknex.h>
+
+STRING* ackcon_str = "AckCon Open Air 2014";
+
+TEXT* ackcon_txt =	
+{
+	layer 	= 	1;
+  	pos_x 	= 	10;
+  	pos_y 	= 	10;
+  	red 	= 	255;
+  	green 	= 	255;
+  	blue 	= 	255;
+  	string 	(ackcon_str);
+  	flags 	= 	SHOW;
+} 
+
+void main (void)
+{
+	level_load(NULL);
+	file_for_screen("shot.tga",0);
+}
