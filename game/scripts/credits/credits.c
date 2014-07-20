7bef36841a9cfa962ab7e0bbccad304ebce009b3
Felix Queißner <felix@ib-queissner.de>
2014-07-20 01:53:04 +0000

    Credits updated.

--- game/scripts/credits/credits.c
+++ game/scripts/credits/credits.c
@@ -23,12 +23,48 @@ MATERIAL *_credits_mtlAlphaTest = {
 	effect = "technique { pass { CullMode = None; AlphaTestEnable = false; AlphaBlendEnable = false; ZEnable = true; ZWriteEnable = true; ZFunc = LessEqual; } }";
 }
 
+MATERIAL *_credits_mtlAlphaAdd = {
+	effect = "technique { pass { CullMode = CCW; AlphaTestEnable = false; AlphaBlendEnable = True; SrcBlend = SrcAlpha; DestBlend = One; ZEnable = true; ZWriteEnable = false; ZFunc = LessEqual; } }";
+}
+
 MATERIAL *_credits_mtlMetal = {
 	effect = "technique { pass { CullMode = None; AlphaTestEnable = false; AlphaBlendEnable = false; ZEnable = true; ZWriteEnable = true; ZFunc = LessEqual; } }";
 	power = 16;
 	specular_blue = 255;
 	specular_green = 230;
 	specular_red = 230;
+}
+
+TEXT *creditsNamePan = {
+	pos_x = 32;
+	pos_y = 32;
+	strings = 1;
+	font = "Courier#64b";
+	flags = TRANSLUCENT;
+}
+
+TEXT *creditsWorkPan1 = {
+	pos_x = 32;
+	pos_y = 32;
+	strings = 1;
+	font = "Courier#48b";
+	flags = TRANSLUCENT | ARIGHT;
+}
+
+TEXT *creditsWorkPan2 = {
+	pos_x = 32;
+	pos_y = 32;
+	strings = 1;
+	font = "Courier#48b";
+	flags = TRANSLUCENT | ARIGHT;
+}
+
+TEXT *creditsWorkPan3 = {
+	pos_x = 32;
+	pos_y = 32;
+	strings = 1;
+	font = "Courier#48b";
+	flags = TRANSLUCENT | ARIGHT;
 }
 
 var _ang_lerp_single(var a1, var a2, var f)
@@ -67,7 +103,6 @@ var creditsBlendSpeed = 0;
 
 void credits_stage() {
 	if(creditsCurrentStage >= credits_stageCount) {
-		error("end!");
 		return;
 	}
 	
@@ -92,31 +127,123 @@ action credits_lottiPlay()
 	}
 }
 
+action credits_lottiLatsch()
+{
+	VECTOR t;
+	t.x = random(1000) - 500;
+	t.y = 350 - random(500);
+	t.z = 26;
+	var offset = random(100);
+	while(me) {
+		
+		if(vec_dist(my.x, t) < 0.1) break;
+		
+		vec_diff(my.pan, t, my.x);
+		vec_normalize(my.pan, minv(10 * time_step, vec_dist(my.x, t)));
+		vec_add(my.x, my.pan);
+		vec_to_angle(my.pan, my.pan);
+		my.roll = 0;
+		
+		ent_animate(me, "walk", 10 * total_ticks + offset, ANM_CYCLE);
+		
+		wait(1);
+	}
+	
+	// Look at stage
+	vec_diff(my.pan, vector(0, 800, 20), my.x);
+	vec_to_angle(my.pan, my.pan);
+	my.roll = 0;
+	
+	while(me) {
+	
+		var time = 16 + random(16);
+		while(time > 0) {
+			ent_animate(me, "stand", 8.9 * total_ticks + offset, ANM_CYCLE);
+			time -= time_step;
+			wait(1);
+		}
+		for(time = 0; time < 60; time += 10 * time_step) {
+			ent_animate(me, "action", time, 0);
+			wait(1);
+		}
+		for(; time > 0; time -= 10 * time_step) {
+			ent_animate(me, "action", time, 0);
+			wait(1);
+		}
+		wait(1);
+	}
+}
+
+int creditsCurrentEntry = 0;
+
+void credits_nextentry() {
+	int count = creditsContent.strings / 4;
+	
+	if(creditsCurrentEntry >= count) {
+		return;
+	}
+	
+	TEXT *texts[4];
+	texts[0] = creditsNamePan;
+	texts[1] = creditsWorkPan1;
+	texts[2] = creditsWorkPan2;
+	texts[3] = creditsWorkPan3;
+	
+	var blendSpeed = 8;
+	
+	int i;
+	for(i = 0; i < 4; i++) {
+		(texts[i].pstring)[0] = (creditsContent.pstring)[4 * creditsCurrentEntry + i];
+		for(texts[i].alpha = 0; texts[i].alpha < 100; texts[i].alpha += blendSpeed * time_step) {
+			wait(1);
+		}
+		texts[i].alpha = 100;
+	}
+	
+	creditsCurrentEntry += 1;
+	wait(-3);
+	
+	for(i = 3; i >= 0; i--) {
+		for(texts[i].alpha = 100; texts[i].alpha > 0; texts[i].alpha -= blendSpeed * time_step) {
+			wait(1);
+		}
+		texts[i].alpha = 0;
+	}
+}
+
 /**
  * Initializes the credits.
  */
 void credits_init()
 {
 	memset(&credits, 0, sizeof(Credits));
-	credits.lottiSpeed = 10;
+	credits.lottiSpeed = 20;
 	
 	credits_initstages();
 	
 	credits_addev(0, credits_stage);
-	//credits_addev(7000, credits_stage);
-	//credits_addev(14000, credits_stage);
-	//credits_addev(21000, credits_stage);
-	//credits_addev(28000, credits_stage);
-	//credits_addev(35000, credits_stage);
-	//credits_addev(42000, credits_stage);
-	//credits_addev(49000, credits_stage);
-	//credits_addev(56000, credits_stage);
-	//credits_addev(63000, credits_stage);
-	//credits_addev(70000, credits_stage);
-	//credits_addev(77000, credits_stage);
-	//credits_addev(84000, credits_stage);
-	//credits_addev(91000, credits_stage);
-	//credits_addev(98000, credits_stage);
+	//credits_addev(3500, credits_nextentry);
+	credits_addev(7000, credits_stage);
+	credits_addev(14000, credits_stage);
+	credits_addev(21000, credits_stage);
+	credits_addev(28000, credits_stage);
+	credits_addev(35000, credits_stage);
+	credits_addev(42000, credits_stage);
+	credits_addev(49000, credits_stage);
+	credits_addev(56000, credits_stage);
+	credits_addev(63000, credits_stage);
+	credits_addev(70000, credits_stage);
+	credits_addev(77000, credits_stage);
+	credits_addev(84000, credits_stage);
+	credits_addev(90000, credits_nextentry);
+	credits_addev(91000, credits_stage);
+	credits_addev(100000, credits_nextentry);
+	
+	int i;
+	int count = creditsContent.strings / 4;
+	for(i = 2; i < count; i++) {
+		credits_addev(110000 + 10000 * (i-2), credits_nextentry);
+	}
 	
 	wait(1);
 	
@@ -124,9 +251,16 @@ void credits_init()
 		// Wait for running credits
 		while(_credits_music == 0) wait(1);
 
+		creditsCurrentEntry = 0;
 		creditsCurrentStage = 0;
 		creditsBlendPerc = 0;
+		
+		creditsNamePan.flags |= SHOW;
+		creditsWorkPan1.flags |= SHOW;
+		creditsWorkPan2.flags |= SHOW;
+		creditsWorkPan3.flags |= SHOW;
 
+		var nextLotti = 128;
 		int currentEvent = 0;
 		int startUpTime = GetTickCount();
 		while(_credits_music != 0) {
@@ -143,15 +277,6 @@ void credits_init()
 				credits_stop();
 				break;
 			}
-			if(key_f) {
-				diag("\n_add_stage(");
-				diag(str_for_num(NULL, camera.x)); diag(", ");
-				diag(str_for_num(NULL, camera.y)); diag(", ");
-				diag(str_for_num(NULL, camera.z)); diag(", ");
-				diag(str_for_num(NULL, camera.pan)); diag(", ");
-				diag(str_for_num(NULL, camera.tilt)); diag(", ");
-				diag(str_for_num(NULL, camera.roll)); diag(", 1);");
-			}
 			
 			if(creditsBlendPerc > 0 && creditsBlendPerc < 100) {
 				creditsBlendPerc = minv(creditsBlendPerc + creditsBlendSpeed * time_step, 100);
@@ -167,9 +292,32 @@ void credits_init()
 				_ang_lerp(camera.pan, creditsFromA, creditsToA, blend);
 			}
 			
+			if(nextLotti <= 0 && num_entities < 80) {
+				if(random(100) < 80) {
+					ent_create("lotter.mdl", vector(random(2000)-1000, -2048, 26), credits_lottiLatsch);
+				} else {
+					ent_create("lottifant.mdl", vector(random(2000)-1000, -2048, 26), credits_lottiLatsch);
+				}
+				nextLotti = 16 + random(32);
+			}
+			
+			creditsWorkPan1.pos_x = screen_size.x - 64;
+			creditsWorkPan2.pos_x = screen_size.x - 64;
+			creditsWorkPan3.pos_x = screen_size.x - 64;
+			
+			creditsWorkPan1.pos_y = screen_size.y - 64 * 1 - 48;
+			creditsWorkPan2.pos_y = screen_size.y - 64 * 2 - 48;
+			creditsWorkPan3.pos_y = screen_size.y - 64 * 3 - 48;
+			
+			nextLotti -= time_step;
+			
 			wait(1);	
 		}
 	
+		creditsNamePan.flags &= ~SHOW;
+		creditsWorkPan1.flags &= ~SHOW;
+		creditsWorkPan2.flags &= ~SHOW;
+		creditsWorkPan3.flags &= ~SHOW;
 	}
 }
 
@@ -189,15 +337,23 @@ void credits_start()
 	stage.pan = 180;
 	stage.material = _credits_mtlAlphaTest;
 	
-	you = ent_create("stage_dach.mdl", vector(0, 500, 20), NULL);
+	you = ent_create("stage_dach.mdl", stage.x, NULL);
 	you.pan = 180;
 	you.material = _credits_mtlAlphaTest;
 	
-	you = ent_create("stage_drums.mdl", vector(0, 500, 20), NULL);
+	you = ent_create("stage_drums.mdl", stage.x, NULL);
 	you.pan = 180;
 	you.material = _credits_mtlAlphaTest;
 	
-	you = ent_create("stage_stangen.mdl", vector(0, 500, 20), NULL);
+	you = ent_create("stage_vorhang.mdl", stage.x, NULL);
+	you.pan = 180;
+	you.material = _credits_mtlAlphaTest;
+	
+	you = ent_create("stage_lichter.mdl", stage.x, NULL);
+	you.pan = 180;
+	you.material = _credits_mtlAlphaAdd;
+	
+	you = ent_create("stage_stangen.mdl", stage.x, NULL);
 	you.pan = 180;
 	you.material = _credits_mtlMetal;
 	
@@ -211,11 +367,16 @@ void credits_start()
 	you.pan = 230;
 	you.material = _credits_mtlAlphaTest;
 	
+	you = ent_create("lotterDrums.mdl", stage.x, credits_lottiPlay);
+	vec_add(you.x, vector(0, 76, 112));
+	you.pan = 270;
+	you.material = _credits_mtlAlphaTest;
+	
 	
 #ifdef TEST_DEBUG
-	_credits_music = media_play("../../media/outro-demo.wav", NULL, 100);
+	_credits_music = media_play("../../media/outro-demo.ogg", NULL, 100);
 #else
-	_credits_music = media_play("media\\outro-demo.wav", NULL, 100);
+	_credits_music = media_play("media\\outro-demo.ogg", NULL, 100);
 #endif
 }
 
--- game/scripts/credits/credits_flow.h
+++ game/scripts/credits/credits_flow.h
@@ -37,11 +37,30 @@ void credits_initstages() {
 	_add_stage(2.029, 661.280, 210.628, 270.368, -13.986, 0, 1);
 	_add_stage(-234.618, 629.067, 208.233, 294.617, -13.986, 0, 1);
 	_add_stage(-439.249, 854.975, 578.169, 296.664, -32.027, 0, 1);
-	_add_stage(-439.249, 854.975, 578.169, 296.664, -32.200, 0, 1);
 	_add_stage(-742.185, -41.973, 472.564, 397.829, -22.253, 0, 1);
 	_add_stage(-267.319, -339.175, 1083.993, 431.818, -60.420, 0, 1);
 	_add_stage(556.372, -232.182, 246.143, 484.703, 11.090, 0, 1);
 	_add_stage(785.279, -562.790, 54.465, 484.703, 12.754, 0, 1);
+}
+
+TEXT *creditsContent = {
+	string(
+		"Jonas Freiknecht", "Armory Supervisor", "Item Supervisor", "Pyrotechnician"
+		"Felix Queissner", "Ingame Selection Maker", "Credit Writer", "",
+		"Andreas Schmidt", "Credit Designer", "", "",
+		"Robert Jäger", "2D Display Maker", "2D Map Minificator", "Chasing Camera Holder",
+		"Christian Behrenberg", "Lower Management", "Sound Effect Voice Actor", "Framework Code Creator",
+		"Marius Kay Appes", "Upper Management", "Music Composer", ""
+		"Michael Schwarz", "Voice Actor", "Level Designer", "",
+		"Simon Trümpler", "Chief 3D Designer", "", "",
+		"Patrick Jendral", "Detail Creationist", "3D Font Maker", "",
+		"Felix Pohl", "Cart Movement Enabler", "Armory Manager", ""
+		"CocaCola", "Gardener", "Cloud Maker", ""
+		"Kanal 21", "Room Provider", "Internet Provider", "",
+		"Achim Lübbeke", "Bielefeld Connections", "", "",
+		"Lottifant", "Missing Link", "", "",
+		"Eselslerche", "Flown Out", "", "",
+		"JCL", "Indirect sponsor of this game", "", "");
 }
 
 #endif // #ifndef _CREDITS_FLOW_H_
